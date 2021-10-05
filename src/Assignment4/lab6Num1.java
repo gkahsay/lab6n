2364a0ad4160081c5b32d5f9b63edab00c8508f1
gkahsay <91564300+gkahsay@users.noreply.github.com>
2021-10-05 16:28:41 -0500

    Initial commit

---
+++ src/Assignment4/lab6Num1.java
@@ -0,0 +1,57 @@
+package Assignment4;
+
+public class lab6Num1 {
+    public static void main(String[] args){
+        //1-a
+        int a=1,b=1,c=1,d=0;
+        if(a==b){
+            if(c==d)  {
+                a=1;
+            }else
+                b=1;
+        }
+        else
+            c=1;
+        //1-b
+        if(a==b){
+            a=1;
+            if(c==d){
+              b=1;
+            }
+            else{
+                c=1;
+            }
+        }
+        //1-c
+        if(a==b) {
+            if (c == d)
+                a = 1;
+            b=1;
+        }
+        else
+            b=1;
+        //1-d
+        if(a==b){
+            if(c==d)
+                a=1;
+            b=2;
+        }
+        else{
+            b=1;
+            if(a==d)
+                d=3;
+        }
+       //2-a
+        //The answer is b & c because both the first if close has match else close
+        //3
+        int e,f,g,h;
+        3-a.false;
+        3-b.false;
+        3-c.true
+        3-d.true
+        3-e.false
+        3-f.true
+        3-g.false
+        3-h.true
+    }
+}
